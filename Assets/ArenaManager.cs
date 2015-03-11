dc6d273a0cd2da37a79ce8104ad8ccce882afc24
mmichaels01 <theomegapoint@gmail.com>
2015-03-11 18:18:54 -0500

    Refactoring
    Moved some code around, renamed classes and functions, made rendering
    more generic

---
+++ Assets/ArenaManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshCollider))]
+public class ArenaManager : MonoBehaviour {
+	public Texture2D tileMap;
+	public int height;
+	public int width;
+
+	MeshFilter filter;
+	MeshCollider collider;
+
+	float texScale = .125f;
+
+	MeshGenerator manager;
+
+	void Start()
+	{
+		filter = GetComponent<MeshFilter>();
+		collider = GetComponent <MeshCollider>();
+		manager = new MeshGenerator(texScale, filter, collider);
+	}
+
+	void FixedUpdate()
+	{
+		manager.GenearateWallsFromTexture(GameObject.Find("WebCamTextureArena").GetComponent<RawImage>().texture as Texture2D,100,100, false);
+	}
+
+	
+
+}
--- Assets/MeshGenerator.cs
+++ Assets/MeshGenerator.cs
@@ -1,120 +1,109 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
 
-[RequireComponent(typeof(MeshFilter))]
-[RequireComponent(typeof(MeshRenderer))]
-[RequireComponent(typeof(MeshCollider))]
-public class MeshGenerator : MonoBehaviour {
-	Texture2D heightMap;
-	public Texture2D tileMap;
+public class MeshGenerator  {
+    public List<Vector3> verts = new List<Vector3>();
+    public List<int> tris = new List<int>();
+    public List<Vector2> uvs = new List<Vector2>();
+    public float texScale;
 
-
-	public int height;
-	public int width;
-
-	MeshFilter filter;
-	MeshCollider collider;
-
-	List<Vector3> verts = new List<Vector3>();
-	List<int> tris = new List<int>();
-	List<Vector2> uvs = new List<Vector2>();
-
-	float texScale = .125f;
-
-	void Start()
-	{
-		heightMap = GameObject.Find("WebCamTextureArena").GetComponent<RawImage>().texture as Texture2D;
-		filter = GetComponent<MeshFilter>();
-		collider = GetComponent <MeshCollider>();
-
-	}
-
-	void FixedUpdate()
-	{
-
-		heightMap = GameObject.Find("WebCamTextureArena").GetComponent<RawImage>().texture as Texture2D;
-
-		for (int x = 0; x < width; x++)
-		{
-			for (int z = 0; z < height; z++)
-			{
-				GenSquare(x, z, heightMap.GetPixel(x, z), new Color(0f / 256f, 0f / 256f, 0f / 256f));
-			}
-		}
-
-		print("h - " + height + " w - " + width);
-		print(verts.Count);
-		GenMesh();
-
-	}
-
-	bool WithinColorRange(Color inputColor, Color targetColor, float strength)
-	{
-		if (Mathf.Abs(targetColor.r - inputColor.r) < strength / 256f
-			&& Mathf.Abs(targetColor.g - inputColor.g) < strength / 256f
-			&& Mathf.Abs(targetColor.b - inputColor.b) < strength / 256f)
-		{
-			return true;
-		}
-
-		return false;
-	}
+    MeshFilter filter;
+    MeshCollider collider;
 
 
-	void GenSquare(int x, int z, Color inputColor, Color targetColor)
-	{
-		//Extrude upward
-		if (WithinColorRange(targetColor, inputColor, 64))
-		{
+    public MeshGenerator(float texScale)
+    {
+        this.texScale = texScale;
+    }
 
-			verts.Add(new Vector3(x, 1, z));
-			verts.Add(new Vector3(x, 1, z + 1));
-			verts.Add(new Vector3(x + 1, 1, z + 1));
-			verts.Add(new Vector3(x + 1, 1, z));
+    public MeshGenerator(float texScale, MeshFilter filter, MeshCollider collider)
+    {
+        this.texScale = texScale;
+        this.filter = filter;
+        this.collider = collider;
+    }
 
-			GenSquareUVs(0, 0);
-			GenSquareTris();
-		}
-	}
+    public void GenearateWallsFromTexture(Texture2D t, int width, int height, bool renderCollision = false){
 
-	void GenSquareTris()
-	{
-		int index = verts.Count - 4;
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                GenerateSquare(x, y, t.GetPixel(x, y), new Color(0f / 256f, 0f / 256f, 0f / 256f));
+            }
+        }
 
-		//First tri
-		tris.Add(index);
-		tris.Add(index + 1);
-		tris.Add(index + 2);
+        GenerateMesh();
 
-		//Second tri
-		tris.Add(index);
-		tris.Add(index + 2);
-		tris.Add(index + 3);
-	}
+    }
 
-	void GenSquareUVs(int texX, int texY)
-	{
-		uvs.Add(new Vector2(texX * texScale, texY * texScale));
-		uvs.Add(new Vector2(texX * texScale, texY * texScale + texScale));
-		uvs.Add(new Vector2(texX * texScale + texScale, texY * texScale + texScale));
-		uvs.Add(new Vector2(texX * texScale + texScale, texY * texScale));
-	}
 
-	void GenMesh()
-	{
-		filter.mesh.Clear();
-		filter.mesh.vertices = verts.ToArray();
-		filter.mesh.triangles = tris.ToArray();
-		filter.mesh.uv = uvs.ToArray();
-		filter.mesh.RecalculateNormals();
+    void GenerateSquare(int x, int z, Color inputColor, Color targetColor)
+    {
+        if (WithinColorRange(targetColor, inputColor, 64))
+        {
 
-		//print("uv" + uvs.Count);
-		//print("vert" + verts.Count);
+            verts.Add(new Vector3(x, 1, z));
+            verts.Add(new Vector3(x, 1, z + 1));
+            verts.Add(new Vector3(x + 1, 1, z + 1));
+            verts.Add(new Vector3(x + 1, 1, z));
 
-		verts.Clear();
-		tris.Clear();
-		uvs.Clear();
-	}
+            GenerateSquareUVs(0, 0);
+            GenerateSquareTris();
+        }
+    }
+
+    public void GenerateCube(int x, int z, Color inputColor, Color targetColor)
+    {
+
+    }
+
+    void GenerateSquareUVs(int texX, int texY)
+    {
+        uvs.Add(new Vector2(texX * texScale, texY * texScale));
+        uvs.Add(new Vector2(texX * texScale, texY * texScale + texScale));
+        uvs.Add(new Vector2(texX * texScale + texScale, texY * texScale + texScale));
+        uvs.Add(new Vector2(texX * texScale + texScale, texY * texScale));
+    }
+
+    void GenerateSquareTris()
+    {
+        int index = verts.Count - 4;
+
+        //First tri
+        tris.Add(index);
+        tris.Add(index + 1);
+        tris.Add(index + 2);
+
+        //Second tri
+        tris.Add(index);
+        tris.Add(index + 2);
+        tris.Add(index + 3);
+    }
+
+    void GenerateMesh()
+    {
+        filter.mesh.Clear();
+        filter.mesh.vertices = verts.ToArray();
+        filter.mesh.triangles = tris.ToArray();
+        filter.mesh.uv = uvs.ToArray();
+        filter.mesh.RecalculateNormals();
+
+        verts.Clear();
+        tris.Clear();
+        uvs.Clear();
+    }
+
+    bool WithinColorRange(Color inputColor, Color targetColor, float strength)
+    {
+        if (Mathf.Abs(targetColor.r - inputColor.r) < strength / 256f
+            && Mathf.Abs(targetColor.g - inputColor.g) < strength / 256f
+            && Mathf.Abs(targetColor.b - inputColor.b) < strength / 256f)
+        {
+            return true;
+        }
+
+        return false;
+    }
 }
