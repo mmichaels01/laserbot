a5f5fc8bc0351d3e4aea49104d1719f1c43017df
ricola22 <wicky000@aim.com>
2015-04-20 12:37:19 -0500

    Updated Sandbox Scenes
    Finished the first three sandbox scenes. More can be added if needed.
    New scenes include the demonstration of the software using our hands,
    single ball interaction with hands, and multi-ball moving with
    interaction with hands.

---
+++ Assets/SandboxScenes/BouncingBall.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BouncingBall : MonoBehaviour {
+
+    Rigidbody rb;
+
+    int rndLow = 15;
+    int rndHigh = 40;
+    Vector3 prevPos;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(Random.Range(rndLow, rndHigh), 0, Random.Range(rndLow, rndHigh));
+	}
+	
+	void FixedUpdate()
+    {
+        if (rb.velocity.x > rndHigh)
+                rb.velocity = new Vector3(rndHigh, 0, rb.velocity.z);
+        if (rb.velocity.x < -rndHigh)
+            rb.velocity = new Vector3(-rndHigh, 0, rb.velocity.z);
+        if (rb.velocity.z > rndHigh)
+                rb.velocity = new Vector3(rb.velocity.x, 0, rndHigh);
+        if (rb.velocity.z < -rndHigh)
+            rb.velocity = new Vector3(rb.velocity.x, 0, -rndHigh);
+            prevPos = transform.position;
+	}
+
+    int RndNum()
+    {
+        return Random.Range(rndHigh-1, rndHigh);
+    }
+
+    void OnTriggerEnter(Collider obj)
+    {
+        if (obj.tag == "VerticalBumper")
+        {
+            print("VerticalBumper");
+            if (rb.velocity.z < 0)
+                rb.velocity = new Vector3(rb.velocity.x, 0, RndNum());
+            else
+                rb.velocity = new Vector3(rb.velocity.x, 0, -RndNum());
+
+            transform.position = new Vector3(transform.position.x, 0.5f, prevPos.z);
+
+        }
+        if (obj.tag == "Bumper")
+        {
+            print("Bumper");
+
+            if (rb.velocity.x < 0)
+                rb.velocity = new Vector3(RndNum(), 0, rb.velocity.z);
+            else
+                rb.velocity = new Vector3(-RndNum(), 0, rb.velocity.z);
+
+            transform.position = new Vector3(prevPos.x, 0.5f, transform.position.z);
+        }
+    }
+}
