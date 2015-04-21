7f26f585d38465d50af77057de1e079d179c01b9
mmichaels01 <theomegapoint@gmail.com>
2015-04-20 22:19:30 -0500

    scene and prefab updates
    scene and prefab updates

---
+++ Assets/TargetList.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TargetList : MonoBehaviour {
+
+    public List<Vector3> positions;
+    public GameObject bot;
+    public float distance;
+    int position;
+
+	// Use this for initialization
+	void Start () {
+        position = 0;
+        transform.position = positions[position];
+	}
+
+    void FixedUpdate()
+    {
+        if (Vector3.Distance(positions[position], bot.transform.position) < distance && position < positions.Count - 1)
+        {
+            position++;
+            transform.position = positions[position];
+            bot.SendMessage("IssueCommand", "mp3,r2_surprise");
+        }
+
+
+    }
+}
